263017ba1179d9a026c589d25859bc1e61d27e2c
priyank <priyankkumar2010@gmail.com>
2022-04-03 12:36:20 +0530

    product nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,28 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { ContactusComponent } from './components/contactus/contactus.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DefaultpageComponent } from './components/defaultpage/defaultpage.component';
-
+import { ProductnavbarComponent } from './components/productnavbar/productnavbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     ContactusComponent,
     NavbarComponent,
-    DefaultpageComponent
+    DefaultpageComponent,
+    ProductnavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/productnavbar/productnavbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductnavbarComponent } from './productnavbar.component';
+
+describe('ProductnavbarComponent', () => {
+  let component: ProductnavbarComponent;
+  let fixture: ComponentFixture<ProductnavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductnavbarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductnavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
