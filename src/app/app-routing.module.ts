8d580aa973071d9006b1c002c6f241337dcb014f
priyank <priyankkumar2010@gmail.com>
2022-04-03 11:34:19 +0530

    modified change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { DefaultpageComponent } from './components/defaultpage/defaultpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'contactus',component :ContactusComponent},
+  {path :'home',component:DefaultpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DefaultpageComponent } from './components/defaultpage/defaultpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    ContactusComponent,
+    NavbarComponent,
+    DefaultpageComponent
   ],
   imports: [
     BrowserModule,
