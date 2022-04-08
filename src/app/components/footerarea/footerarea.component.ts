1524b001a30696f601875e772388cdcbe8192d2e
priyank <priyankkumar2010@gmail.com>
2022-04-08 23:09:36 +0530

    some msg

--- src/app/components/footerarea/footerarea.component.ts
+++ src/app/components/footerarea/footerarea.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footerarea',
@@ -7,9 +8,18 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 })
 export class FooterareaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-  
+  onFBClick() {
+    window.location.href = "https://www.facebook.com/angel_printing_house-105220155200674/";
+  }
+  onInstaClick(){
+    window.location.href = "https://www.instagram.com/angel_printing_house/";
+  }
+
+  onWhatsAppClick(){
+    window.location.href  ="https://api.whatsapp.com/send?phone=917488003670&text=Welcome%20to%20Angel%20printing%20house";
+  }
 }
